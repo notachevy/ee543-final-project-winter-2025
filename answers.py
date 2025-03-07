aba9d74fe2c62fc26ba6d48989a10ecccd19964e
notachevy <66398554+notachevy@users.noreply.github.com>
2025-03-07 13:54:22 -0800

    symbolic

--- answers.py
+++ answers.py
@@ -1,4 +1,38 @@
 from symbolic import *
 
 if __name__ == "__main__":
-    t_01 = Link_S(0, 0, t1, 0)
+    t1, t2, t3, t4 = sp.symbols("t1 t2 t3 t4")
+    # Link_S(alpha, a, theta, d)
+    t_01 = Link_S(0,    0,      t1,  62.8)
+    t_12 = Link_S(90,   0,      t2,  0)
+    t_23 = Link_S(0,    101,    t3,  0)
+    t_34 = Link_S(90,   0,      t4,  87.5)
+    t_45 = Link_S(0,    0,      0,  125)
+
+    t_05 = t_01 @ t_12 @ t_23 @ t_34 @ t_45
+
+    print("Symbolic Representation")
+    
+    print("T_01")
+    sp.pprint(t_01)
+    print()
+
+    print("T_12")
+    sp.pprint(t_12)
+    print()
+
+    print("T_23")
+    sp.pprint(t_23)
+    print()
+
+    print("T_34")
+    sp.pprint(t_34)
+    print()
+
+    print("T_45")
+    sp.pprint(t_45)
+    print()
+    
+    print("T_05")
+    sp.pprint(t_05)
+    print()
